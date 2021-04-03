34f92e10f0950ef30192c8f40b5c0b1b49800c26
Bruno Silva <bruno.vp.1999@gmail.com>
2021-04-03 14:22:04 +0100

    Old Styuff

---
+++ website/scripts/cryptojs/all.js
@@ -0,0 +1,20 @@
+var Crypto = exports.Crypto = require('./lib/Crypto').Crypto;
+
+/*[ 'CryptoMath'
+, 'BlockModes'
+, 'DES'
+, 'AES'
+, 'HMAC'
+, 'MARC4'
+, 'MD5'
+, 'PBKDF2'
+, 'PBKDF2Async'
+, 'Rabbit'
+, 'SHA1'
+, 'SHA256'
+].forEach( function (path) {
+	require('./lib/' + path);
+});*/
+
+var SHA256 = require('./lib/SHA256');
+var bs58 = require('bs58');
