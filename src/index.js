bd3ef710971b7b8132758808af2ccc20ba8c1329
Gabriel Cardoso <55664632+ItsDuty@users.noreply.github.com>
2021-04-04 19:37:34 +0100

    aa

--- src/index.js
+++ src/index.js
@@ -4,12 +4,28 @@ import * as qr from "qrcode";
 const crypto = require('crypto');
 
 window.privKeyGen = function(){
-
-    var privKeyHex = crypto.randomBytes(32).toString('hex').toUpperCase();
-    document.getElementById("privKeyHex").innerHTML = privKeyHex;
+    //Gerar private key
+    var privKeyArray = crypto.randomBytes(32);
+    var privKeyHex = privKeyArray.toString('hex');
+    document.getElementById("privKeyHex").innerHTML = privKeyHex.toUpperCase();
     qr.toCanvas(document.getElementById('privKeyHexQR'), privKeyHex, function(error){
         if(error) {console.error(error)}
     })
+    //Gerar WIF Mainnet
+    var vers_mainnet = '80';
+    var firstSHA256 = crypto.createHash('sha256').update(privKeyArray).digest('hex');
+    console.log(privKeyHex);
+    console.log(firstSHA256);
+
+    //Gerar WIF Testnet
+    var vers_testnet = 'EF';
+
+
+
+
+
+
+
     //var privKeyStruct = btc.ECPair.makeRandom({ compressed: false, network: btc.networks.bitcoin });
     //console.log(privKeyStruct);  //DEBUG
     //var privKey = privKeyStruct.privateKey;
