1a97d08440fb0bcd6bd9135097229be6736befe6
Gabriel Cardoso <55664632+ItsDuty@users.noreply.github.com>
2021-04-04 12:45:25 +0100

    qrcodes

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 "use strict";
-//var btc = require('bitcoinjs-lib');
-//var openssl = require('openssl');
-/*import * as btc from "bitcoinjs-lib";*/
-
-const btc = require('bitcoinjs-lib');
-
-
-//document.getElementById("wif").innerHTML = wif.toUpperCase();
-
-
-//console.log(privKey.privateKey);
-//var wif = privKey.toWIF();
-//console.log(wif);
-//document.getElementById("wif").innerHTML = wif;
-
+import * as btc from "bitcoinjs-lib";
+import * as qr from "qrcode";
 
 window.privKeyGen = function(){
-    var privKey = btc.ECPair.makeRandom({ compressed: false, network: btc.networks.bitcoin });   //makeRandom(btc.networks.bitcoin);
-    console.log(privKey);
-    var privKeyHex = privKey.privateKey.toString('hex').toUpperCase();
-    var wif = privKey.toWIF();
+
+    var privKeyStruct = btc.ECPair.makeRandom({ compressed: false, network: btc.networks.bitcoin });
+    console.log(privKeyStruct);  //DEBUG
+
+    var privKey = privKeyStruct.privateKey;
+    var privKeyHex = privKey.toString('hex').toUpperCase();
     document.getElementById("privKeyHex").innerHTML = privKeyHex;
+    qr.toCanvas(document.getElementById('privKeyHexQR'), privKeyHex, function(error){
+        if(error) {console.error(error)}
+        console.log('Success!');
+    })
+    
+    var wif = privKeyStruct.toWIF();
     document.getElementById("wif").innerHTML = wif;
+    qr.toCanvas(document.getElementById('wifQR'), wif, function(error){
+        if(error) {console.error(error)}
+        console.log('Success!');
+    })
+
 };
