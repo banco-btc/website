0af82e88422913faa72513f71d55db37b669a7aa
Gabriel Cardoso <55664632+ItsDuty@users.noreply.github.com>
2021-04-04 15:45:17 +0100

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,25 +1,26 @@
 "use strict";
 import * as btc from "bitcoinjs-lib";
 import * as qr from "qrcode";
+const crypto = require('crypto');
+import 'bootstrap/dist/css/bootstrap.css';
 
 window.privKeyGen = function(){
 
-    var privKeyStruct = btc.ECPair.makeRandom({ compressed: false, network: btc.networks.bitcoin });
-    console.log(privKeyStruct);  //DEBUG
-
-    var privKey = privKeyStruct.privateKey;
-    var privKeyHex = privKey.toString('hex').toUpperCase();
+    var privKeyHex = crypto.randomBytes(32).toString('hex').toUpperCase();
     document.getElementById("privKeyHex").innerHTML = privKeyHex;
     qr.toCanvas(document.getElementById('privKeyHexQR'), privKeyHex, function(error){
         if(error) {console.error(error)}
-        console.log('Success!');
     })
+    //var privKeyStruct = btc.ECPair.makeRandom({ compressed: false, network: btc.networks.bitcoin });
+    //console.log(privKeyStruct);  //DEBUG
+    //var privKey = privKeyStruct.privateKey;
+    //var privKeyHex = privKey.toString('hex').toUpperCase();
     
-    var wif = privKeyStruct.toWIF();
-    document.getElementById("wif").innerHTML = wif;
-    qr.toCanvas(document.getElementById('wifQR'), wif, function(error){
-        if(error) {console.error(error)}
-        console.log('Success!');
-    })
+    //var wif = privKeyStruct.toWIF();
+    //document.getElementById("wif").innerHTML = wif;
+    //qr.toCanvas(document.getElementById('wifQR'), wif, function(error){
+    //    if(error) {console.error(error)}
+    //    console.log('Success!');
+    //})
 
 };
