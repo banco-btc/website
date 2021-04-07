6754bc5237e13fd592954327fa8468a7cb2ef417
Gabriel Cardoso <55664632+ItsDuty@users.noreply.github.com>
2021-04-08 00:35:27 +0100

    rtrthr

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Button } from 'react-bootstrap';
-import { PrivKeyGen } from "./PrivKeyGen";
 
 export default function App() {
   return (
     <div className="App">
-      <div>
-        <h2>Clica aqui para criar uma Private Key</h2>
-        <Button onClick={PrivKeyGen} id="btn_pkg" variant="link">
-          <img src={logo} className="Btc-spinner" alt="logo" />
-        </Button>{' '}
-        <div id="pk_h"></div>
-        <canvas id="pk_h_qr"></canvas>
-      </div>
+      
     </div>
   );
 }
--- src/Main.js
+++ src/Main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './Main.css';
-import { Container } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 import PrivKeyGen from "./Components/PrivKeyGen";
 
 export default function Main() {
   return (
     <Container fluid="md" className="Main">
-      <PrivKeyGen />
+      <Row>
+        <Col><PrivKeyGen /></Col>
+        <Col></Col>
+      </Row>
+      
     </Container>
   );
 }
