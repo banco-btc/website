71f0554d906117aa9de56027e67d86c85de21914
Gabriel Cardoso <55664632+ItsDuty@users.noreply.github.com>
2021-04-07 12:38:26 +0100

    h56

--- src/Header.js
+++ src/Header.js
@@ -1,40 +1,40 @@
 import { Alert } from 'bootstrap';
+import React from 'react';
 import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 let btc_price = require('crypto-price');
 
 export default function Header() {
-    return (
-      <div className="Header">
-        <Navbar bg="light" expand="md">
-          <Navbar.Brand href="#home">Banco BTC</Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="mr-auto">
-              <span id="btc_price"></span>
-              <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#link">Link</Nav.Link>
-              <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
-            <Form inline>
-              <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-              <Button variant="outline-success">Search</Button>
-            </Form>
-          </Navbar.Collapse>
-        </Navbar>
-      </div>
-    );
+  
+  const printar = () => {
+    return('Olá');
   }
 
+  return (
+    <div className="Header">
+      <Navbar bg="light" expand="md">
+        <Navbar.Brand href="#home">Banco BTC</Navbar.Brand>
+        <h2>{printar()}</h2>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <span id="btc_price"></span>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+  }
 
-btc_price.getCryptoPrice('EUR', 'BTC').then(obj => {
-    document.getElementById("btc_price").innerHTML = Math.round(obj.price * 100) / 100;
-}).catch(err => {
-    console.log(err);
-})
-console.log(obj);
