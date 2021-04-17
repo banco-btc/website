3b769dece681ff718748a3053f91e0cd14b1235b
Gabriel Cardoso <55664632+ItsDuty@users.noreply.github.com>
2021-04-17 15:40:40 +0100

    sobre nos

--- src/Main.js
+++ src/Main.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Css/Main.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import PrivKeyGen from "./Components/PrivKeyGen";
+import Sobrenos from "./Components/Sobrenos";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from './Components/Home';
 import SideInfo from './Components/SideInfo';
@@ -18,6 +19,7 @@ export default function Main() {
                 <Switch>
                   <Route path="/" exact component={Home} />
                   <Route path="/single_wallet" component={PrivKeyGen} />
+                  <Route path="/sobrenos" component={Sobrenos} />
                 </Switch>
               </AnimatePresence>
             </Col>
